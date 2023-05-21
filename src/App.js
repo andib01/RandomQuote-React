7c8170a60614fac43c2ab5979065c4c81c6d3bec
andib01 <126267515+andib01@users.noreply.github.com>
2023-05-22 00:29:14 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  // http://api.quotable.io/random
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
 
   useEffect(() => {
-    fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);  
-          setAuthor(quote.author);
-        }
-      )
-  },[]);
+    fetch('https://cors-anywhere.herokuapp.com/http://api.quotable.io/random')
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      })
+      .catch((error) => {
+        console.error('Error fetching quote:', error);
+      });
+  }, []);
 
-  let fetchNewQuote = () => {
-    fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);  
-          setAuthor(quote.author);
-        }
-      )
-  }
+  const fetchNewQuote = () => {
+    fetch('https://cors-anywhere.herokuapp.com/http://api.quotable.io/random')
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      })
+      .catch((error) => {
+        console.error('Error fetching quote:', error);
+      });
+  };
+
   return (
     <div className="App">
-         <div className="quote">
-            <h2>{quote}</h2>
-            <small>-{author}-</small>
-         </div><br />
-         <button className="btn" onClick={fetchNewQuote}>Generate New Quote</button>
+      <div className="quote">
+        <h2>{quote}</h2>
+        <small>-{author}-</small>
+      </div>
+      <br />
+      <button className="btn" onClick={fetchNewQuote}>
+        Generate New Quote
+      </button>
     </div>
   );
 }
